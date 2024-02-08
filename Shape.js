d3b1b0c9e778a360fde039eb3c982f927fbb6ac7
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-02-07 18:57:27 -0600

    created parent class Shape and added child shapes, added createLogo function to index.js

---
+++ Shape.js
@@ -0,0 +1,10 @@
+// Parent class to be inherited by children classes 
+class Shape {
+    constructor(shapeColor, text, textColor) {
+        this.shapeColor = shapeColor;
+        this.text = text;
+        this.textColor = textColor;
+    }
+};
+
+module.exports = Shape
--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const questions = require('./lib/questions.js');
+const setShape = require('./lib/setShape.js')
+
+// Creating new SVG file using input from inquirer
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+}
+
+// Initialize questions then createLogo with responses and catch any errors
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+        })
+    .catch(err => {
+            console.log(err)
+        });
+}
+
+init();
